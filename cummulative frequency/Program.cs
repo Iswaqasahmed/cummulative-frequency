994b9b8c1f69900ce1031f8be9b95a8fb60dc9f2
waqas ahmed <37307194+waqasahmed123@users.noreply.github.com>
2019-01-18 16:58:41 +0500

    cummulative frequency file

---
+++ cummulative frequency/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cummulative_frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("To Enter the Number & Find the Cummulative Frequency ");
+            Console.WriteLine("---------------------------------------------------- ");
+            int max, min;
+            Console.WriteLine("Enter the Minium Number of Frequency");
+            min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Maximum Number of Frequency");
+            max = int.Parse(Console.ReadLine());
+            int n;
+            int[] a = new int[10]; //{1,1,1,2,4,5,6,7,8};
+            Console.WriteLine("Enter value");
+            for (int i = min; i <= max; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                a[i] = n;
+            }
+            int a1 = 1, b = 0;
+            for (int j = min; j <= max; j++)
+            {
+
+                for (int i = 1; i <= 9; i++)
+                {
+
+                    if (a1 == a[i])
+                    {
+                        b = ++b;
+                    }
+
+
+                }
+                ++a1;
+
+                Console.WriteLine("No of {0}'s = {1} ", j, b);
+                b = 0;
+
+            }
+        }
+    }
+}
